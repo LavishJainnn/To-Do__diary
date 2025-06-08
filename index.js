9fe17e4eac25f1fefeaa52fc0c1db98288f4ea81
Lavish Jain <lavishjain30306@gmail.com>
2025-06-09 03:37:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+var items = [];
+
+app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.render("view", {arr: items});
+});
+
+app.post("/", function(req,res){
+    var item = req.body.element;
+    items.push(item);
+    res.redirect("/");
+});
+
+app.post("/delete", function(req,res){
+    var index = req.body.delete;
+    items.splice(index,1);
+    res.redirect("/");
+});
+
+app.listen(27,function(){
+    console.log("Server Started.");
+});
